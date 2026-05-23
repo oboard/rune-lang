857bdb87ecbe2e8b9c331659225ce8d14c91d371
oboard <oboard@outlook.com>
2026-05-23 13:16:14 +0800

    feat: add test for nested void match functionality in interpreter

---
+++ internal/interpreter/interpreter_test.go
@@ -0,0 +1,41 @@
+package interpreter
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/oboard/rune-lang/internal/compiler"
+)
+
+func TestInterpreterRunsNestedVoidMatch(t *testing.T) {
+	src := `nestedMatch() => {
+  x := 1
+  y := 2
+  x {
+    1 => y {
+      2 => @io.println("x is 1 and y is 2")
+      _ => @io.println("x is 1 and y is not 2")
+    }
+    _ => @io.println("x is not 1")
+  }
+}
+
+main() => {
+  nestedMatch()
+}
+`
+	prog, diags := compiler.AnalyzeSource("nested_match.rn", src)
+	if len(diags) > 0 {
+		t.Fatalf("diagnostics = %#v, want none", diags)
+	}
+
+	var out bytes.Buffer
+	interp := New(prog.IR, WithOutput(&out))
+	if err := interp.RunMain(); err != nil {
+		t.Fatalf("RunMain() error = %v", err)
+	}
+	if got := strings.TrimSpace(out.String()); got != "x is 1 and y is 2" {
+		t.Fatalf("output = %q, want nested match output", got)
+	}
+}
